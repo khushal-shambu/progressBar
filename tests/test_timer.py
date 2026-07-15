8a1b7ab816e779a53c87a4a318db93922c8992fe
KhushalShambu <kushalshambu@gmail.com>
2026-07-15 17:45:12 -0400

    Wrote test cases to ensure critical functions do not break going forward. Also changed the requirements.txt. github actions were also added to this to ensure code does break in production when pushed to github.

---
+++ tests/test_timer.py
@@ -0,0 +1,12 @@
+def test_90_minutes():
+    minutes = 90
+    hours = minutes / 60
+
+    assert hours == 1.5
+
+
+def test_30_minutes():
+    minutes = 30
+    hours = minutes / 60
+
+    assert hours == 0.5
